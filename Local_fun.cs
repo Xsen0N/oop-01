48b2d80489e6e9d2f89af9c28b835def3e4191e7
Ksenia Zhuk <kseni.zhyk.3@gmail.com>
2022-09-12 20:32:54 +0300

    add the task with a local function

---
+++ Local_fun.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq; //LINQ (Language-Integrated Query) представляет простой и удобный язык запросов к источнику данных.
+
+namespace Local_fun
+{
+    class Local_fun
+    {
+        static void Main(string[] args)
+        {
+            (int, int, int, char) localFun(int[] mass, string st)
+            {
+                int max = mass.Max();
+                int min = mass.Min();
+                int sum = mass.Sum();
+                char sim = st.First();
+                return (max, min, sum, sim);
+            }
+            int[] mass = { 1, 2, 3, 4, 5 };
+            string st = "Hello, World";
+            Console.WriteLine(localFun(mass, st));
+        }
+    }
+}
