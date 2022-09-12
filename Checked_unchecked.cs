9a326b11f8add558d0b378b0157d5cb0dd3fcb9e
Ksenia Zhuk <kseni.zhyk.3@gmail.com>
2022-09-12 20:34:28 +0300

    add last task

---
+++ Checked_unchecked.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace checked_unchecked
+{
+    class Checked_unchecked
+    {
+        static void Main(string[] args)
+        {
+            int Fun1()
+            {
+                checked // проверяется на переполнение, происходит обработка исключений
+                {
+                    int x = int.MaxValue;
+                    try
+                    {
+                        return x + 1;
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("Переполнение");
+                        return x;
+                    }
+                }
+            }
+
+            int Fun2()
+            {
+                unchecked
+                {
+                    int x = int.MaxValue;
+                    try
+                    {
+                        return x + 1;
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("Переполнение");
+                        return x;
+                    }
+                }
+            }
+            Console.WriteLine(Fun1());
+            Console.WriteLine(Fun2());
+        }
+    }
+}
